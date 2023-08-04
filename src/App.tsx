56acf4be9fb51be58b6dafca7cfd63354611720f
coreykadd <coreymangan22@live.ie>
2023-08-04 14:49:00 +0100

    List group component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import ListGroup from './components/ListGroup';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const handleSelectItem = (item: string) => {
+        console.log('item > ', item);
+    };
+
+    return (
+        <div>
+            <ListGroup items={items} heading="Items" onSelectItem={handleSelectItem} />
+        </div>
+    );
 }
 
-export default App
+export default App;
