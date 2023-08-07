dda81c2af4510b4b974b2d731c7dcf36bb3a72b9
coreykadd <coreymangan22@live.ie>
2023-08-07 14:45:10 +0100

    Alert and Button components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
+import { useState } from 'react';
 import './App.css';
-import ListGroup from './components/ListGroup';
+import Alert from './components/Alert';
+import Button from './components/Button';
+// import ListGroup from './components/ListGroup';
 
 function App() {
-    const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+    const [alertVisible, setAlertVisibility] = useState(false);
+    // const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
 
-    const handleSelectItem = (item: string) => {
-        console.log('item > ', item);
-    };
+    // const handleSelectItem = (item: string) => {
+    //     console.log('item > ', item);
+    // };
 
     return (
         <div>
-            <ListGroup items={items} heading="Items" onSelectItem={handleSelectItem} />
+            { alertVisible &&
+                <Alert onClose={() => setAlertVisibility(false)}>
+                    Yes <span>ya</span>
+                </Alert>
+            }
+
+            <Button onClick={() => setAlertVisibility(true)}>A button</Button>
+            {/* <ListGroup items={items} heading="Items" onSelectItem={handleSelectItem} /> */}
         </div>
     );
 }
